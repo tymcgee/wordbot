bb876c9a687bd3cdb631bb2f0303432410513dfb
tynan <tynanmcgeek@gmail.com>
2024-02-19 21:10:06 -0800

    refactor: factor out guess validation

--- main.go
+++ main.go
@@ -26,19 +26,10 @@ func main() {
 	for {
 		var guess string
 		if scanner.Scan() {
-			inp := scanner.Text()
-			inpSplit := strings.Split(inp, " ")
-			if len(inpSplit) > 1 || len(inpSplit) == 0 || len(inpSplit[0]) != 5 {
-				fmt.Println("Guess must be exactly five letters")
-				continue
-			}
-			guess = inpSplit[0]
-
-			// not sure I'm understanding slices vs arrays properly, but as far
-			// as I can tell, this essentially just passes a reference to the
-			// underlying array, and does not copy it, which is what I want.
-			if !slices.Contains(VALID_GUESSES[:], guess) {
-				fmt.Println("Invalid guess")
+			input := scanner.Text()
+			var isValid bool
+			isValid, guess = validateGuess(input)
+			if !isValid {
 				continue
 			}
 		}
@@ -51,4 +42,23 @@ func main() {
 		}
 		guessNum++
 	}
+}
+
+func validateGuess(input string) (bool, string) {
+	inpSplit := strings.Split(input, " ")
+	if len(inpSplit) > 1 || len(inpSplit) == 0 || len(inpSplit[0]) != 5 {
+		fmt.Println("Guess must be exactly five letters")
+		return false, ""
+	}
+	guess := inpSplit[0]
+
+	// not sure I'm understanding slices vs arrays properly, but as far
+	// as I can tell, this essentially just passes a reference to the
+	// underlying array, and does not copy it, which is what I want.
+	if !slices.Contains(VALID_GUESSES[:], guess) {
+		fmt.Println("Invalid guess")
+		return false, ""
+	}
+
+	return true, guess
 }
