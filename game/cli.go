6f032a0af6899a0c30a4827cc1e621aebd686d7b
tynan <tynanmcgeek@gmail.com>
2024-02-20 21:05:10 -0800

    refactor: move cli code into its own file

---
+++ game/cli.go
@@ -0,0 +1,54 @@
+package game
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"slices"
+	"strings"
+)
+
+func PlayCli() {
+	g := Game{
+		ShowStats:        true,
+		ShowOngoingStats: true,
+	}
+	g.PlayGame(getGuessFromCli)
+}
+
+func getGuessFromCli() string {
+	scanner := bufio.NewScanner(os.Stdin)
+	var guess string
+	var isValid bool
+	for {
+		if scanner.Scan() {
+			input := scanner.Text()
+			isValid, guess = validateGuess(input)
+			if !isValid {
+				continue
+			}
+			// valid guess
+			break
+		}
+	}
+	return guess
+}
+
+func validateGuess(input string) (bool, string) {
+	inpSplit := strings.Split(input, " ")
+	if len(inpSplit) > 1 || len(inpSplit) == 0 || len(inpSplit[0]) != 5 {
+		fmt.Println("Your guess must be exactly five letters")
+		return false, ""
+	}
+	guess := inpSplit[0]
+
+	// not sure I'm understanding slices vs arrays properly, but as far
+	// as I can tell, this essentially just passes a reference to the
+	// underlying array, and does not copy it, which is what I want.
+	if !slices.Contains(VALID_GUESSES[:], guess) {
+		fmt.Println("Invalid guess")
+		return false, ""
+	}
+
+	return true, guess
+}
--- game/game.go
+++ game/game.go
@@ -1,12 +1,9 @@
 package game
 
 import (
-	"bufio"
 	"fmt"
 	"math/rand"
-	"os"
 	"slices"
-	"strings"
 )
 
 const (
@@ -18,32 +15,6 @@ const (
 type Game struct {
 	ShowStats        bool
 	ShowOngoingStats bool
-}
-
-func PlayCli() {
-	g := Game{
-		ShowStats:        true,
-		ShowOngoingStats: true,
-	}
-	g.PlayGame(getGuessFromCli)
-}
-
-func getGuessFromCli() string {
-	scanner := bufio.NewScanner(os.Stdin)
-	var guess string
-	var isValid bool
-	for {
-		if scanner.Scan() {
-			input := scanner.Text()
-			isValid, guess = validateGuess(input)
-			if !isValid {
-				continue
-			}
-			// valid guess
-			break
-		}
-	}
-	return guess
 }
 
 func (g *Game) PlayGame(getGuess func() string) {
@@ -117,23 +88,4 @@ func getColors(guess []rune, answer []rune) string {
 		colors += color
 	}
 	return colors
-}
-
-func validateGuess(input string) (bool, string) {
-	inpSplit := strings.Split(input, " ")
-	if len(inpSplit) > 1 || len(inpSplit) == 0 || len(inpSplit[0]) != 5 {
-		fmt.Println("Your guess must be exactly five letters")
-		return false, ""
-	}
-	guess := inpSplit[0]
-
-	// not sure I'm understanding slices vs arrays properly, but as far
-	// as I can tell, this essentially just passes a reference to the
-	// underlying array, and does not copy it, which is what I want.
-	if !slices.Contains(VALID_GUESSES[:], guess) {
-		fmt.Println("Invalid guess")
-		return false, ""
-	}
-
-	return true, guess
 }
