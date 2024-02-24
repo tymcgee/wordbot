281415166fab29fabbd2cdd9b3702ac7ee3df098
tynan <tynanmcgeek@gmail.com>
2024-02-24 13:54:57 -0800

    feat: add command line flags

--- main.go
+++ main.go
@@ -1,18 +1,31 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 
 	"github.com/tymcgee/wordbot/bot"
+	"github.com/tymcgee/wordbot/game"
 )
 
 func main() {
-	// game.PlayCli()
+	var isBot bool
+	var answer string
+	var numOfGames int
+	flag.BoolVar(&isBot, "bot", false, "whether to use the bot")
+	flag.StringVar(&answer, "a", "", "predefined answer")
+	flag.IntVar(&numOfGames, "n", 1, "number of games to play (only works in conjunction with --bot)")
+	flag.Parse()
+
+	if !isBot {
+		game.PlayCli(answer)
+		return
+	}
+
 	numGuessesOnWinningGames := 0
 	numGuesses := 0
 	wins := 0
-	totalGames := 1000
-	for i := range totalGames {
+	for i := range numOfGames {
 		fmt.Printf("\rPlaying game %d", i+1)
 		results := bot.BotGame(answer, bot.FilterGuesses)
 		numGuesses += results.Guesses
@@ -21,8 +34,8 @@ func main() {
 			wins++
 		}
 	}
-	fmt.Printf("\nPlayed %d games\n", totalGames)
-	fmt.Printf("Win percentage is ~%.2f%%\n", (float64(wins)/float64(totalGames))*100)
-	fmt.Printf("Average number of guesses when winning is %.3f\n", float64(numGuessesOnWinningGames)/float64(totalGames))
-	fmt.Printf("Average number of guesses overall is %.3f\n", float64(numGuesses)/float64(totalGames))
+	fmt.Printf("\nPlayed %d games\n", numOfGames)
+	fmt.Printf("Win percentage is ~%.2f%%\n", (float64(wins)/float64(numOfGames))*100)
+	fmt.Printf("Average number of guesses when winning is %.3f\n", float64(numGuessesOnWinningGames)/float64(numOfGames))
+	fmt.Printf("Average number of guesses overall is %.3f\n", float64(numGuesses)/float64(numOfGames))
 }
