c062f300cfaffb46dad6a6c39403355fe5a1546d
tynan <tynanmcgeek@gmail.com>
2024-02-24 12:45:06 -0800

    refactor: factor out filter method into smaller parts

--- bot/filter.go
+++ bot/filter.go
@@ -9,94 +9,110 @@ type GameInformation struct {
 	Index  int
 }
 
+// Strategy where you don't filter any, so every guess becomes random
 func FilterNone(validGuesses []string, gray []GameInformation, yellow []GameInformation, green []GameInformation) []string {
 	return validGuesses
 }
 
+// If we know where a green is, all valid guesses must have that letter in that spot.
+// Grays are a bit trickier because of double letters.
+// If we know where a yellow is, valid guesses must have that letter in the guess, but it
+// can't be in the same place as last time.
 func FilterGuesses(validGuesses []string, gray []GameInformation, yellow []GameInformation, green []GameInformation) []string {
 	newValidGuesses := make([]string, 0)
 
-	// fmt.Printf("validGuesses: %v\n", validGuesses)
-
 	for _, guess := range validGuesses {
-		isValid := true
 		guessRunes := []rune(guess)
-		for _, greenInfo := range green {
-			// essentially hardmode -- if a letter was green,
-			// it must be green again in the next guess
-			if guessRunes[greenInfo.Index] != greenInfo.Letter {
-				// fmt.Printf("invalid guess [%v] because of green [%v]\n", guess, string(greenInfo.Letter))
-				isValid = false
-				break
-			}
+
+		// GREEN
+		if !areGreensValid(guessRunes, gray, yellow, green) {
+			continue
 		}
 
-		// double letters can have a gray but still have that letter in the word..
-		// for example, "speed" with the first e green and second e gray
-		for _, grayInfo := range gray {
-			if !slices.Contains(guessRunes, grayInfo.Letter) {
-				continue
-			}
-
-			// now the only case when it IS valid is if it maps to a green or yellow
-			// that we already know about
-			if len(green) == 0 && len(yellow) == 0 {
-				// fmt.Printf("invalid guess [%v] because gray + no green [%v]\n", guess, string(grayInfo.Letter))
-				isValid = false
-				break
-			}
-
-			// check yellow
-			isYellow := false
-			for _, yellowInfo := range yellow {
-				if yellowInfo.Letter == grayInfo.Letter {
-					isYellow = true
-				}
-			}
-			if isYellow {
-				// we are good, this is an okay case
-				continue
-			}
-
-			// check green
-			indices := getIndices(guessRunes, grayInfo.Letter)
-			isInGreen := false
-			for _, i := range indices {
-				for _, greenInfo := range green {
-					if greenInfo.Index == i {
-						isInGreen = true
-					}
-				}
-			}
-
-			if !isInGreen {
-				// fmt.Printf("invalid guess [%v] because gray [%v]\n", guess, string(grayInfo.Letter))
-				isValid = false
-				break
-			}
+		// GRAY
+		if !areGraysValid(guessRunes, gray, yellow, green) {
+			continue
 		}
 
-		for _, yellowInfo := range yellow {
-			// also hardmode -- must contain a letter that was previously yellow
-			if !slices.Contains(guessRunes, yellowInfo.Letter) {
-				// fmt.Printf("invalid guess [%v] because doesn't contain yellow letter [%v]\n", guess, string(yellowInfo.Letter))
-				isValid = false
-				break
-			}
-			// don't put the same letter in the same spot twice
-			if guessRunes[yellowInfo.Index] == yellowInfo.Letter {
-				// fmt.Printf("invalid guess [%v] because yellow letter in same spot twice [%v]\n", guess, string(yellowInfo.Letter))
-				isValid = false
-				break
-			}
+		// YELLOW
+		if !areYellowsValid(guessRunes, gray, yellow, green) {
+			continue
 		}
 
-		if isValid {
-			newValidGuesses = append(newValidGuesses, guess)
-		}
+		newValidGuesses = append(newValidGuesses, guess)
 	}
 
 	return newValidGuesses
+}
+
+func areGreensValid(guess []rune, gray []GameInformation, yellow []GameInformation, green []GameInformation) bool {
+	for _, greenInfo := range green {
+		// essentially hardmode -- if a letter was green,
+		// it must be green again in the next guess
+		if guess[greenInfo.Index] != greenInfo.Letter {
+			return false
+		}
+	}
+	return true
+}
+
+func areGraysValid(guess []rune, gray []GameInformation, yellow []GameInformation, green []GameInformation) bool {
+	// double letters can have a gray but still have that letter in the word..
+	// for example, "speed" with the first e green or yellow and second e gray
+	for _, grayInfo := range gray {
+		if !slices.Contains(guess, grayInfo.Letter) {
+			continue
+		}
+
+		// now the only case when it IS valid is if it maps to a green or yellow
+		// that we already know about
+		if len(green) == 0 && len(yellow) == 0 {
+			return false
+		}
+
+		// check yellow
+		// (the case where it's a double letter and we already have a yellow clue for that letter)
+		isYellow := false
+		for _, yellowInfo := range yellow {
+			if yellowInfo.Letter == grayInfo.Letter {
+				isYellow = true
+			}
+		}
+		if isYellow {
+			// we are good, this is an okay case
+			continue
+		}
+
+		// check green
+		indices := getIndices(guess, grayInfo.Letter)
+		isInGreen := false
+		for _, i := range indices {
+			for _, greenInfo := range green {
+				if greenInfo.Index == i {
+					isInGreen = true
+				}
+			}
+		}
+
+		if !isInGreen {
+			return false
+		}
+	}
+	return true
+}
+
+func areYellowsValid(guess []rune, gray []GameInformation, yellow []GameInformation, green []GameInformation) bool {
+	for _, yellowInfo := range yellow {
+		// also hardmode -- must contain a letter that was previously yellow
+		if !slices.Contains(guess, yellowInfo.Letter) {
+			return false
+		}
+		// don't put the same letter in the same spot twice
+		if guess[yellowInfo.Index] == yellowInfo.Letter {
+			return false
+		}
+	}
+	return true
 }
 
 func getIndices(letters []rune, key rune) []int {
