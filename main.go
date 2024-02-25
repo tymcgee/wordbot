045c0ea76f93685b328138e4803eed77abb11010
tynan <tynanmcgeek@gmail.com>
2024-02-24 16:04:11 -0800

    feat: add argument validation for words

--- main.go
+++ main.go
@@ -3,10 +3,14 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"regexp"
 
 	"github.com/tymcgee/wordbot/bot"
 	"github.com/tymcgee/wordbot/game"
 )
+
+var IsAlpha = regexp.MustCompile(`^[a-zA-Z]+$`).MatchString
 
 func main() {
 	var isBot bool
@@ -19,10 +23,14 @@ func main() {
 	flag.StringVar(&firstGuess, "first", "", "first guess to use (only works in conjunction with --bot) (default first guess is random)")
 	flag.Parse()
 
+	validateWord(answer)
+
 	if !isBot {
 		game.PlayCli(answer)
 		return
 	}
+
+	validateWord(firstGuess)
 
 	numGuessesOnWinningGames := 0
 	numGuesses := 0
@@ -40,4 +48,12 @@ func main() {
 	fmt.Printf("Win percentage is ~%.2f%%\n", (float64(wins)/float64(numOfGames))*100)
 	fmt.Printf("Average number of guesses when winning is %.3f\n", float64(numGuessesOnWinningGames)/float64(numOfGames))
 	fmt.Printf("Average number of guesses overall is %.3f\n", float64(numGuesses)/float64(numOfGames))
+}
+
+// Exits if the word is not five alpha characters or empty.
+func validateWord(word string) {
+	if (len(word) != 0 && len(word) != 5) || !IsAlpha(word) {
+		fmt.Printf("\"%v\" is an invalid word\n", word)
+		os.Exit(1)
+	}
 }
